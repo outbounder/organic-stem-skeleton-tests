0849a366d1d6ad17119cf3e381288483398f7d77
outbounder <obiwonn@gmail.com>
2016-01-18 22:06:18 +0200

    initial commit

---
+++ test/client/react-flux.test.js
@@ -0,0 +1,40 @@
+describe('react-flux', function () {
+  var stemCell = new StemSkeleton(require(process.cwd() + '/stemskeleton.json'))
+  before(function (next) {
+    stemCell.mockTestFolder(next)
+  })
+  before(function (next) {
+    stemCell.stackUpgrade('devtools', next)
+  })
+  before(function (next) {
+    stemCell.stackUpgrade('devtools-client', next)
+  })
+  before(function (next) {
+    stemCell.stackUpgrade('devtools-webpack', next)
+  })
+  before(function (next) {
+    stemCell.stackUpgrade('devtools-less', next)
+  })
+  before(function (next) {
+    stemCell.stackUpgrade('devtools-assets', next)
+  })
+  before(function (next) {
+    stemCell.stackUpgrade('react-flux', next)
+  })
+  after(function (next) {
+    stemCell.removeMockedFolder(next)
+  })
+  it('$angel build', function (next) {
+    stemCell.exec('angel build', function (err, stdout) {
+      if (err) return next(err)
+      next()
+    })
+  })
+  it('$angel watch', function (next) {
+    var child = stemCell.exec('angel watch')
+    child.on('data', function (chunk) {
+      // TODO buffer until expect successfully
+      stemCell.forceExit(child, next)
+    })
+  })
+})
